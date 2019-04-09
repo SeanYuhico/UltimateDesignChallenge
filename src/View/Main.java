84f63224c7188511ff6b8d4f7762de1738029af2
JerickWong <42172510+JerickWong@users.noreply.github.com>
2019-04-09 16:50:37 +0800

    ayosed

--- src/View/Main.java
+++ src/View/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         mainStage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("View/LoginArtist.FXML"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/LoginArtist.FXML"));
         mainStage.setScene(new Scene(root, 599, 417));
     }
 
